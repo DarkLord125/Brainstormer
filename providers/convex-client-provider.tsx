4e06fd43cdab8745af6f1a2d8cfc326684c92985
Ashwin Nair <hashwin381@gmail.com>
2024-07-13 17:56:00 +0530

    New: Integrate Clerk for handling user sign-ups and Setup Convex configuration and connect to the project

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
+import { Loading } from "@/components/auth/Loading";
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
